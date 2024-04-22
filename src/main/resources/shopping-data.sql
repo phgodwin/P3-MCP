e7d4c400db37f112683d88b212d9926d2cc0a89f
Craigm84 <craigmorris2907@gmail.com>
2024-04-22 11:58:46 +0100

    craig branch

--- src/main/resources/shopping-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `cart` VALUES (default, 'Test business');
-INSERT INTO `cart` VALUES (default, 'Test business 2');
-INSERT INTO `cart` VALUES (default, 'Test business 3');
-INSERT INTO `item` VALUES (default, 'Test course', 10.99, 10, null);
-INSERT INTO `item` VALUES (default, 'Test course 2', 120.50, 10, null);
-INSERT INTO `item` VALUES (default, 'Test course 3', 10.99, 10, null);
-INSERT INTO `item` VALUES (default, 'Test course 4', 10.99, 10, 3);
